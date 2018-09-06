dc48097e0105ad8696e20795a2894f93db557290
Hunter Jarrell <hjarrell555@gmail.com>
2018-09-06 15:37:47 -0400

    Generate devise models for Students and Professors.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+  devise_for :professors
+  devise_for :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
