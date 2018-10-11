5c54f7e9755c5d1c9457a4a1043486c98fa81cb3
ErolB <eroljb@hotmail.com>
2018-10-11 18:54:27 -0400

    able to add courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -69,6 +69,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:course_code, :title, :description, :professor_id_id)
+      params.require(:course).permit(:course_code, :title, :description)
     end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,2 @@
 class Course < ApplicationRecord
-  belongs_to :professor_id
 end
