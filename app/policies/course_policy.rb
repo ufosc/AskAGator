12ec9ff1244664bc0b137091768e0acd54704176
Hunter Jarrell <hjarrell555@gmail.com>
2019-02-05 17:17:01 -0500

    Remove broken course policy

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -1,23 +1,2 @@
 class CoursePolicy < ApplicationPolicy
-
-    def new?
-        user.professor?
-    end
-
-    def edit?
-        user.professor?
-    end
-
-    def create?
-        user.professor?
-    end
-
-    def update?
-        user.professor?
-    end
-
-    def destroy?
-        user.admin?
-    end
-
 end
