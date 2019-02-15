4c7906bad166b6bff3e9fc2ebb370864ea5f3ac8
Hunter Jarrell <hjarrell555@gmail.com>
2019-02-14 23:46:33 -0500

    Add test accounts to seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,7 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias AskAGator.Accounts.User
+
+AskAGator.Repo.insert! User.changeset(%User{}, %{name: "Test", email: "test@test.com", password: "password", password_confirmation: "password"})
+AskAGator.Repo.insert! User.changeset(%User{}, %{name: "Test2", email: "test2@test.com", password: "password", password_confirmation: "password"})
