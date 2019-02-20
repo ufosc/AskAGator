0ab526aa358d50e176ba986015ec3137aa4439b9
Hunter Jarrell <hjarrell555@gmail.com>
2019-02-19 19:36:03 -0500

    Add UI for LoginPage
    This shows how to use TypeScript and Material-UI. Also
    have a function stub for handling the actual calls to
    sign in.

--- assets/js/pages/LoginPage.tsx
+++ assets/js/pages/LoginPage.tsx
@@ -1,14 +1,99 @@
 import * as React from 'react'
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { WithStyles, createStyles, Theme, withStyles } from '@material-ui/core';
 
-class LoginPage extends React.Component {
-    render() {
-        return (
-            <div>
-              <h2>Login Page</h2>
-              <p>Hello from the login page!</p>
-            </div>
-        );
+const styles = (theme: Theme) => createStyles({
+    main: {
+        width: 'auto',
+        display: 'block', // Fix IE 11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing.unit,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
+});
+
+interface Props extends WithStyles<typeof styles> {
+}
+
+const LoginPage = withStyles(styles)(
+    class extends React.Component<Props> {
+
+        signIn() {
+            console.log("Sign in")
+        }
+
+        render() {
+            const { classes } = this.props;
+            return (
+                <main className={classes.main}>
+                    <CssBaseline />
+                    <Paper className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                        Sign in
+                        </Typography>
+                        <form className={classes.form}>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="email">Email Address</InputLabel>
+                            <Input id="email" name="email" autoComplete="email" autoFocus />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input name="password" type="password" id="password" autoComplete="current-password" />
+                        </FormControl>
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={this.signIn}
+                        >
+                            Sign in
+                        </Button>
+                        </form>
+                    </Paper>
+                    </main>
+            );
+        }
     }
-};
+);
 
 export default LoginPage
