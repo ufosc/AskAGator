31af4b420b6b4a6167c85d8671dcebb9d6cd636d
Eric <ericnavar@ufl.edu>
2019-03-21 17:37:29 -0400

    Copied and pasted code from LogIn into SignUp but removed the remember me checkbox and changed every Sign In to Sign up

--- assets/js/pages/SignUpPage.tsx
+++ assets/js/pages/SignUpPage.tsx
@@ -1,14 +1,94 @@
 import * as React from 'react'
-import gql from "graphql-tag";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { WithStyles, createStyles, Theme, withStyles } from '@material-ui/core';
 
-class SignUpPage extends React.Component {
-    render() {
-        return (
-            <div>
-            	<h2>Sign Up Page</h2>
-            </div>
-        );
+const styles = (theme: Theme) => createStyles({
+    main: {
+        width: 'auto',
+        display: 'block', // Fix IE 11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing.unit,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
+});
+
+interface Props extends WithStyles<typeof styles> {
+}
+
+const SignUpPage = withStyles(styles)(
+    class extends React.Component<Props> {
+
+        signUp() {
+            console.log("Sign up")
+        }
+
+        render() {
+            const { classes } = this.props;
+            return (
+                <main className={classes.main}>
+                    <CssBaseline />
+                    <Paper className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                        Sign up
+                        </Typography>
+                        <form className={classes.form}>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="email">Email Address</InputLabel>
+                            <Input id="email" name="email" autoComplete="email" autoFocus />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input name="password" type="password" id="password" autoComplete="current-password" />
+                        </FormControl>
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            onClick={this.signUp}
+                        >
+                            Sign up
+                        </Button>
+                        </form>
+                    </Paper>
+                    </main>
+            );
+        }
     }
-};
+);
 
 export default SignUpPage
