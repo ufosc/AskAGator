a33839b1d4998933efa5f74ef79c59f52dc56327
Hunter Jarrell <hjarrell555@gmail.com>
2019-03-22 21:14:44 -0400

    Actually fix code climate and coveralls

--- mix.exs
+++ mix.exs
@@ -12,7 +12,8 @@ defmodule AskAGator.MixProject do
       aliases: aliases(),
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]    ]
+      preferred_cli_env: ["coveralls.json": :test]
+    ]
   end
 
   # Configuration for the OTP application.
