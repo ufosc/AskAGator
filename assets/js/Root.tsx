c32010ebf91fb2d587779933c1c972b8420efe65
Hunter Jarrell <hjarrell555@gmail.com>
2019-03-26 18:03:16 -0400

    Remove old bearer auth code

--- assets/js/Root.tsx
+++ assets/js/Root.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom'
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
-import { AUTH_TOKEN } from './constants';
-
 import HomePage from './pages/index';
 import LoginPage from './pages/LoginPage';
 import Header from './components/Header';
@@ -13,14 +11,6 @@ const client = new ApolloClient({
   uri: 'http://localhost:4000/api',
   fetchOptions: {
     credentials: 'include'
-  },
-  request: async (operation) => {
-    const token = await localStorage.getItem(AUTH_TOKEN);
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      }
-    });
   },
   onError: ({ graphQLErrors, networkError }) => {
     // Do something with the errors lol
