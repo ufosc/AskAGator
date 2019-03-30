5a25b8b32c4ae61d90ab4d7f1aba6fc74a86ccd0
Eric <ericnavar@ufl.edu>
2019-03-30 13:12:36 -0400

    Changed SignUpPage to look like LoginPage but without the remember me check box

--- assets/js/pages/SignUpPage.tsx
+++ assets/js/pages/SignUpPage.tsx
@@ -3,7 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
