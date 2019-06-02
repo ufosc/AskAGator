13aaa0626bc87e1a25ebbb2518d91db1945f95f5
Hunter Jarrell <hjarrell555@gmail.com>
2019-06-02 18:22:10 -0400

    Add jest deps and testing for Redux Actions

---
+++ assets/js/store/actions/auth.test.tsx
@@ -0,0 +1,22 @@
+import * as AuthActions from './auth';
+import { User } from '../../models/user';
+
+describe('AuthActions', () => {
+    it('should create an action to login a User', () => {
+        const user : User = { email: "test@test.com", exists: true, firstName: "Test", lastName: "Test" };
+        const expectedAction = {
+            type: AuthActions.AUTH_ACTIONS.LOGIN_ACT,
+            user
+        };
+
+        expect(AuthActions.loginAct(user)).toStrictEqual(expectedAction);
+    })
+
+    it('should create an action to logout a User', () => {
+        const expectedAction = {
+            type: AuthActions.AUTH_ACTIONS.LOGOUT_ACT
+        };
+
+        expect(AuthActions.logoutAct()).toStrictEqual(expectedAction);
+    })
+})
