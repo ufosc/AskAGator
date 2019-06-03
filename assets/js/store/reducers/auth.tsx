dc7b8b4f41d9596c32209c10f99cfdd0424aa2ab
Hunter Jarrell <hjarrell555@gmail.com>
2019-06-02 23:35:20 -0400

    Fix style on reducers/auth.tsx

--- assets/js/store/reducers/auth.tsx
+++ assets/js/store/reducers/auth.tsx
@@ -1,14 +1,14 @@
-import { AUTH_ACTIONS } from '../actions/auth';
+import { AUTH_ACTIONS } from "../actions/auth";
 
-const auth = (state = [], action : any) => {
-    switch (action.type) {
-        case AUTH_ACTIONS.LOGIN_ACT:
-            return action.user;
-        case AUTH_ACTIONS.LOGOUT_ACT:
-            return { exists: false }
-        default:
-            return state
-    }
+const auth = (state = [], action: any) => {
+  switch (action.type) {
+    case AUTH_ACTIONS.LOGIN_ACT:
+      return action.user;
+    case AUTH_ACTIONS.LOGOUT_ACT:
+      return { exists: false };
+    default:
+      return state;
+  }
 };
 
 export default auth;
