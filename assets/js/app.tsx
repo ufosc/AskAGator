6699d886d378ffa73909fb67c076158e84bdc1f7
Hunter Jarrell <hjarrell555@gmail.com>
2019-06-02 23:31:56 -0400

    Fix style on app.tsx

--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.css"
+import "../css/app.css";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,17 +9,17 @@ import "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
 
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
-import Root from './Root'
+import Root from "./Root";
 
 // This code starts up the React app when it runs in a browser. It sets up the routing
 // configuration and injects the app into a DOM element.
-ReactDOM.render(<Root />, document.getElementById('react-app'))
+ReactDOM.render(<Root />, document.getElementById("react-app"));
