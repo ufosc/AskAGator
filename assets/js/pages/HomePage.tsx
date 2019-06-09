cf7cb156735e0eded1bba54e60fd1df95e180ee5
Hunter Jarrell <hjarrell555@gmail.com>
2019-06-09 17:08:49 -0400

    Remove all users  query

--- assets/js/pages/HomePage.tsx
+++ assets/js/pages/HomePage.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react'
 
-import AllUsers from '../components/AllUsers'
 import Main from '../components/Main'
 
 class HomePage extends React.Component {
     public render() {
         return (
             <Main>
-                Hello These are all the users retrieved using a GraphQL Query!!!!!
-                <AllUsers/>
+                Hello
             </Main>
         );
     }
